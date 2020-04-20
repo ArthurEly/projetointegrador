d6558811b7913850443fc69c2b8f53284ca6c3d3
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-20 16:04:49 -0300

    Commeteu suicidio

--- app/src/main/java/br/com/senac/projetointegrador/MainActivity.java
+++ app/src/main/java/br/com/senac/projetointegrador/MainActivity.java
@@ -1,23 +1,16 @@
 package br.com.senac.projetointegrador;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.app.*;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void trocar (View view)
-    {
-        Intent intent = new Intent(this, SplashActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
