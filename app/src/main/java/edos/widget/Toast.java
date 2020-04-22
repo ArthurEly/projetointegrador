59ebaea108bdc46e1d5d47cddbe3155bb0535123
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-22 02:18:05 -0300

    Commit Automatico

--- app/src/main/java/edos/widget/Toast.java
+++ app/src/main/java/edos/widget/Toast.java
@@ -6,7 +6,7 @@ import android.view.*;
 
 public class Toast extends android.widget.Toast {
 	public Toast(Activity ctx, int xml, int tempo) {
-		super(ctx);
+		super((Context) ctx);
 		View layout = ctx.getLayoutInflater().inflate(xml,null,true);
 		setDuration(tempo);
 		setView(layout);
