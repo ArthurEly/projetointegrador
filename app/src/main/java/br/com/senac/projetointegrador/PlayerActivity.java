5d79315d2af54d19bb6a005539b0d829cc3e6d0c
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2020-04-21 22:00:17 -0300

    Commit Automatico

---
+++ app/src/main/java/br/com/senac/projetointegrador/PlayerActivity.java
@@ -0,0 +1,11 @@
+package br.com.senac.projetointegrador;
+import android.app.*;
+import android.os.*;
+
+public class PlayerActivity extends Activity {
+
+	@Override protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_player);
+	}
+}
